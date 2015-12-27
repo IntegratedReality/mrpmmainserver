b638510b358172b40be7196100bbcdf80882abae
tapioka <tapioka.euler@gmail.com>
2015-12-27 16:43:48 +0900

    make base of projection mapping

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,16 +6,14 @@ void ofApp::setup() {
     ofSetFrameRate(60);
     ofSetVerticalSync(true);
     ofEnableSmoothing();
-    
-    img.loadImage("quad_warp_kittens.png");
 
-    int x = (ofGetWidth() - img.getWidth()) * 0.5;       // center on screen.
-    int y = (ofGetHeight() - img.getHeight()) * 0.5;     // center on screen.
-    int w = img.getWidth();
-    int h = img.getHeight();
-    
+    int x = (ofGetWidth() - projectionWidth) * 0.5;       // center on screen.
+    int y = (ofGetHeight() - projectionHeight) * 0.5;     // center on screen.
+    int w = projectionWidth;
+    int h = projectionHeight;
+
     fbo.allocate(w, h);
-    
+
     warper.setSourceRect(ofRectangle(0, 0, w, h));              // this is the source rectangle which is the size of the image and located at ( 0, 0 )
     warper.setTopLeftCornerPosition(ofPoint(x, y));             // this is position of the quad warp corners, centering the image on the screen.
     warper.setTopRightCornerPosition(ofPoint(x + w, y));        // this is position of the quad warp corners, centering the image on the screen.
@@ -32,65 +30,65 @@ void ofApp::update()
         // only update every 5 frames.
         return;
     }
-    
+
     for(int i=0; i<10; i++) {
         // randomise points over the image area.
-        points[i].x = ofRandom(img.getWidth());
-        points[i].y = ofRandom(img.getHeight());
+        points[i].x = ofRandom(projectionWidth);
+        points[i].y = ofRandom(projectionHeight);
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
     ofSetColor(255);
-    
+
     //======================== draw image into fbo.
-    
+    // edit here to draw object
     fbo.begin();
-    img.draw(0, 0);
+    ofCircle(50, 50, 20);
     fbo.end();
-    
+
     //======================== get our quad warp matrix.
-    
+
     ofMatrix4x4 mat = warper.getMatrix();
-    
+
     //======================== use the matrix to transform our fbo.
-    
+
     ofPushMatrix();
     ofMultMatrix(mat);
     fbo.draw(0, 0);
     ofPopMatrix();
-    
+
     //======================== use the matrix to transform points.
 
     ofSetLineWidth(2);
     ofSetColor(ofColor::cyan);
-    
+
     for(int i=0; i<9; i++) {
         int j = i + 1;
-        
+
         ofVec3f p1 = mat.preMult(ofVec3f(points[i].x, points[i].y, 0));
         ofVec3f p2 = mat.preMult(ofVec3f(points[j].x, points[j].y, 0));
-        
+
         ofDrawLine(p1.x, p1.y, p2.x, p2.y);
     }
-    
+
     //======================== draw quad warp ui.
-    
+
     ofSetColor(ofColor::magenta);
     warper.drawQuadOutline();
-    
+
     ofSetColor(ofColor::yellow);
     warper.drawCorners();
-    
+
     ofSetColor(ofColor::magenta);
     warper.drawHighlightedCorner();
-    
+
     ofSetColor(ofColor::red);
     warper.drawSelectedCorner();
-    
+
     //======================== info.
-    
+
     ofSetColor(ofColor::white);
     ofDrawBitmapString("to warp the image, drag the corners of the image.", 20, 30);
     ofDrawBitmapString("press 's' to toggle quad warp UI. this will also disable quad warp interaction.", 20, 50);
@@ -108,11 +106,11 @@ void ofApp::keyPressed(int key) {
     if(key == 's' || key == 'S') {
         warper.toggleShow();
     }
-    
+
     if(key == 'l' || key == 'L') {
         warper.load();
     }
-    
+
     if(key == 'h' || key == 'H') {
         warper.save();
     }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxQuadWarp.h"
 
 class ofApp : public ofBaseApp{
 public:
     void setup();
     void update();
     void draw();
+    void exit();
 
     void keyPressed(int key);
     void keyReleased(int key);
@@ -14,16 +16,17 @@ public:
     void mouseDragged(int x, int y, int button);
     void mousePressed(int x, int y, int button);
     void mouseReleased(int x, int y, int button);
-    void mouseEntered(int x, int y);
-    void mouseExited(int x, int y);
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
 
     ofxQuadWarp warper;
 
-    ofImage img;
+    ofRectangle rect;
     ofFbo fbo;
+
+    int projectionWidth = 600;
+    int projectionHeight = 400;
 
     ofPoint points[10];
 };
